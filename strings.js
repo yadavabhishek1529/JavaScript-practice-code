819b4c9a81d7c958970870a02d4a1b16085419da
Abhishek yadav <yadavabhishek1529@gmail.com>
2026-05-11 22:57:18 +0530

    Strings & it's methods concept done

--- strings.js
+++ strings.js
@@ -22,3 +22,45 @@ console.log(specialString);
 let output = `The cost of ${obj.name} is ${obj.price} rupees.`;
 console.log(output);   
 */
+
+/* let str = "abhi";
+console.log(str.toUpperCase()); */
+/*
+let str = "ABHI YADAV";
+console.log(str.toLowerCase());*/
+
+//trim() method
+/*
+let str = "    abhi yadav    ";
+console.log(str);
+console.log(str.trim());
+*/
+
+//concat() method
+/*
+let str1 = "Abhi";
+let str2 = "yadav";
+
+let res = str1.concat(str2);
+console.log(res); */
+
+//slice() method
+/* let str = "Abhishek yadav";
+let res = str.slice(4,8);
+console.log(res); */
+
+//replace() method
+/* let str = "Abhishek yadav";
+let res = str.replace("i", "e");
+console.log(res); */ 
+
+//charAt() method
+/* let str = "Abhi"
+let res = str.charAt(2);
+console.log(res); */
+/*
+let name = prompt("enter your name");
+
+let userName = "@"+ name + name.length;
+alert(userName); */
+
