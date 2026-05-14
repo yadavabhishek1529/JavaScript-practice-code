2ff9676bcc899d02a0a872a777d9ba7a478c794e
Abhishek yadav <yadavabhishek1529@gmail.com>
2026-05-14 18:33:47 +0530

    arrow functions concept done

--- functions.js
+++ functions.js
@@ -108,3 +108,14 @@ x.forEach((i) => {
 })
 */
 
+const countVowels = (str) => {
+    let count = 0;
+    for(let char of str){
+        if(char == "A" || char == "E" || char == "I" || char == "O" || char == "U" || char == "a" || char == "e" || char == "i" || char == "o" || char == "u") {
+            count ++;
+        }
+    }
+    console.log(count);
+}
+
+countVowels("Abhishek Yadav");  // output : 5
