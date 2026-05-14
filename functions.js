bb7cb0bbc70f17eb8fef352aa68a6de140518448
Abhishek yadav <yadavabhishek1529@gmail.com>
2026-05-14 19:07:28 +0530

    arrow functions

--- functions.js
+++ functions.js
@@ -49,6 +49,20 @@ printHello();*/
 }
 countVowels("Abhishek Yadav");  */
 
+/*
+const countVowels = (str) => {
+    let count = 0;
+    for(let char of str){
+        if(char == "A" || char == "E" || char == "I" || char == "O" || char == "U" || char == "a" || char == "e" || char == "i" || char == "o" || char == "u") {
+            count ++;
+        }
+    }
+    console.log(count);
+}
+
+countVowels("Abhishek Yadav");  // output : 5
+*/
+
     //using arrow function:-
 /* const countVowels = (str) => {
     let count = 0;
@@ -107,15 +121,3 @@ x.forEach((i) => {
     console.log("Hii");
 })
 */
-
-const countVowels = (str) => {
-    let count = 0;
-    for(let char of str){
-        if(char == "A" || char == "E" || char == "I" || char == "O" || char == "U" || char == "a" || char == "e" || char == "i" || char == "o" || char == "u") {
-            count ++;
-        }
-    }
-    console.log(count);
-}
-
-countVowels("Abhishek Yadav");  // output : 5
