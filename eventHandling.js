dfa97836a84b412c301edad9cbf3ea34b4c5905f
Abhishek yadav <yadavabhishek1529@gmail.com>
2026-05-17 23:16:15 +0530

    Event Handling concept done

--- eventHandling.js
+++ eventHandling.js
@@ -85,21 +85,3 @@ let btn = document.querySelector("#btn");
 btn.onclick = () =>{
     console.log("button is clicked");
 } */
-
-let modeBtn = document.querySelector("#btn");
-let body = document.querySelector("body");
-let currMode = "light";
-
-modeBtn.addEventListener("cick" , () => {
-    if(currMode === "light"){
-        currMode = "dark";
-        body.classList.add("dark");
-        body.classList.remove("light");
-    }
-    else{
-        currMode = "light";
-        body.classList.add("light");
-        body.classList.remove("dark")
-    }
-    console.log(currMode);
-})
