ce8e26f8e466cfabcba79ef54667776730e7fce9
Abhishek yadav <yadavabhishek1529@gmail.com>
2026-05-20 17:22:17 +0530

    classes and objects concept done

--- classesAndObjects.js
+++ classesAndObjects.js
@@ -257,26 +257,3 @@ class admin extends user{
     }
 }
 let admin1 = new admin("admin","admin@college.edu.in"); */
-
-//try-catch method for error handling.
-/* let a = 10;
-let b = 5;
-
-console.log("a =",a);
-console.log("b =",b);
-console.log("a+b =",a+b);
-console.log("a-b =",a-b);
-try{
-console.log("a+c =",a+c);
-}
-catch(err){
-    console.log(err);
-}
-console.log("a*b =",a*b);
-console.log("a/b =",a/b);
-console.log("a+b =",a+b);
-console.log("a-b =",a-b);
-console.log("a+b =",a+b);
-console.log("a+b =",a+b);
-console.log("a+b =",a+b);    
-*/
