f95b6911f7d0384263ffd70a47256048505b1b6d
Abhishek yadav <yadavabhishek1529@gmail.com>
2026-05-20 17:23:58 +0530

    error handling concept done

---
+++ errorhandling.js
@@ -0,0 +1,22 @@
+//try-catch method for error handling.
+/* let a = 10;
+let b = 5;
+
+console.log("a =",a);
+console.log("b =",b);
+console.log("a+b =",a+b);
+console.log("a-b =",a-b);
+try{
+console.log("a+c =",a+c);
+}
+catch(err){
+    console.log(err);
+}
+console.log("a*b =",a*b);
+console.log("a/b =",a/b);
+console.log("a+b =",a+b);
+console.log("a-b =",a-b);
+console.log("a+b =",a+b);
+console.log("a+b =",a+b);
+console.log("a+b =",a+b);    
+*/
