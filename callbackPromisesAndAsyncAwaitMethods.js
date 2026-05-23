8de3b0f0c132eabed11b2a7a41a7fe060c518207
Abhishek yadav <yadavabhishek1529@gmail.com>
2026-05-23 22:49:03 +0530

    practicing promises concept

--- callbackPromisesAndAsyncAwaitMethods.js
+++ callbackPromisesAndAsyncAwaitMethods.js
@@ -224,6 +224,23 @@ getData(1, () =>{
     });
 });
 */
+/*
+const getPromise = () =>{
+    return new Promise((resolve,reject)=>{
+        console.log("it is a promise");
+        // resolve("success");
+        reject("network error");
+    });
+};
+
+let promise = getPromise();
+promise.then((res)=>{
+    console.log("promise fulfilled",res);
+})
+
+promise.catch((err)=>{
+    console.log("promise rejected",err);
+}) */
 
 const getPromise = () =>{
     return new Promise((resolve,reject)=>{
