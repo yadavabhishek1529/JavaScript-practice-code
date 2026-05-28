17f59b738651e4038e9348f4c180642ad31c7d12
Abhishek yadav <yadavabhishek1529@gmail.com>
2026-05-28 18:31:23 +0530

    Callback hell, Promises and async-await concept done

--- callbackPromisesAndAsyncAwaitMethods.js
+++ callbackPromisesAndAsyncAwaitMethods.js
@@ -285,3 +285,40 @@ asyncFunc1().then((res)=>{
     asyncFunc2().then((res)=>{});
 });
 */
+
+/* function api() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Weather Data");
+            resolve(200);
+        },3000);
+    })
+}
+
+async function getWeatherData() {
+    await api();
+    await api();
+}
+// getWeatherData(); 
+*/
+
+/* function getData(dataId){
+    return new Promise((resolve, reject) => {
+        setTimeout(()=>{
+            console.log("Data i'd : ",dataId);
+            resolve(200);
+        },2000);
+    })
+}
+
+async function getAllData() {
+    console.log("getting data 1...");
+    await getData(1);
+    console.log("getting data 2...");
+    await getData(2);
+    console.log("getting data 3...");
+    await getData(3);
+}
+
+// getAllData();
+*/
